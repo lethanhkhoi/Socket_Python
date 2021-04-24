34389d44fdd3aef1a64a73f6769507d9379ed4a3
lethanhkhoi <63887994+lethanhkhoi@users.noreply.github.com>
2021-04-24 16:29:34 +0700

    update

--- client/main.py
+++ client/main.py
@@ -3,10 +3,17 @@ import sys
 import os
 import socket
 import threading
+import pyautogui
+import subprocess
+import wmi
+import time
 
-
-from PySide2.QtWidgets import QApplication, QWidget,QPushButton,QMessageBox, QLineEdit, QTableWidget
-from PySide2.QtWidgets import QListWidget
+import PIL.Image
+import PIL.ImageTk
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,QRadialGradient)
+from PySide2.QtWidgets import *
+from PySide2.QtWidgets import QListWidget,QFileDialog
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtGui import QIcon
@@ -14,14 +21,17 @@ from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
 
-class client(QWidget):
+
+class Form(QWidget):
     def __init__(self):
-        super(client, self).__init__()
+        super(Form, self).__init__()
         self.setWindowTitle("client")
         self.load_ui()
         self.set_icon()
+        self.client_ = None
+        self.signal = False #check xem ket noi chua
         self.set_Button()
-        self.create()
+
 
 
     def load_ui(self):
@@ -45,6 +55,8 @@ class client(QWidget):
         self.pbutton = self.findChild(QPushButton,"Process_R_Button")
         self.kbutton = self.findChild(QPushButton,"Key_Stroke_Button")
         self.rbutton = self.findChild(QPushButton,"Fix_Reg_Button")
+        self.picbutton = self.findChild(QPushButton,"Pic_Button")
+        self.shutdown = self.findChild(QPushButton,"Turn_off_Button")
 
         self.cbutton.clicked.connect(self.check)
         self.ebutton.clicked.connect(self.exit_)
@@ -52,35 +64,61 @@ class client(QWidget):
         self.pbutton.clicked.connect(self.process_r)
         self.kbutton.clicked.connect(self.keystroke_r)
         self.rbutton.clicked.connect(self.reg)
+        self.picbutton.clicked.connect(self.take_pic)
+        self.shutdown.clicked.connect(self.turn_off)
+    def turn_off(self):
+        if self.client_ == None:
+            self.message()
+        elif self.client_ !=None:
+            shut_down(self.client_)
+    def take_pic(self):
+        if self.client_ == None:
+            self.message()
+        elif self.client_ !=None:
+            self.picture = Pic(self.client_)
+            self.picture.show()
 
     def reg(self):
-        self.regis = Registry()
-        self.regis.show()
+        if self.client_ == None:
+            self.message()
+        elif  self.client_ != None:
+            self.regis = Registry(self.client_)
+            self.regis.show()
 
     def keystroke_r(self):
-        self.key = Keystroke()
-        self.key.show()
+        if self.client_ == None:
+            self.message()
+        elif  self.client_ != None:
+            self.key = Keystroke(self.client_)
+            self.key.show()
 
     def process_r(self):
-        self.list_app = List_App()
-        self.list_app.show()
+        if self.client_ == None:
+            self.message()
+        elif  self.client_ != None:
+            self.list_app = Process(self.client_)
+            self.list_app.show()
 
     def app_r(self):
-        self.list_app = List_App()
-        self.list_app.show()
+        if self.client_ == None:
+            self.message()
+        elif  self.client_ != None:
+            self.list_app = Process(self.client_)
+            self.list_app.show()
 
     def exit_(self):
         exit()
 
-    def check(self):
+    def check(self,client_):
         self.ipline = self.findChild(QLineEdit,"lineEdit").text()
         try:
-            create(self.ipline)
+            self.client_ = create(self.ipline)
+            if self.client_ != None:
+                self.signal = True
             msg = QMessageBox()
             msg.setWindowTitle("Success")
             msg.setText("Kết nối thành công")
             x = msg.exec_()
-
         except OSError:
             self.message()
     def message(self):
@@ -91,8 +129,9 @@ class client(QWidget):
         x = msg.exec_()
 
 class kill(QWidget):
-    def __init__(self):
+    def __init__(self,Client):
         super(kill, self).__init__()
+        self.client_ = Client
         self.setWindowTitle("Kill")
         self.load_ui()
 
@@ -104,9 +143,15 @@ class kill(QWidget):
         loader.load(ui_file,self)
         ui_file.close()
 
+        self.killbutton = self.findChild(QPushButton,"Kill_Button")
+        self.killbutton.clicked.connect(self.check)
+    def check(self):
+        exit()
+
 class start(QWidget):
-    def __init__(self):
+    def __init__(self,Client):
         super(start,self).__init__()
+        self.client_ = Client
         self.setWindowTitle("Start")
         self.load_ui()
 
@@ -118,13 +163,15 @@ class start(QWidget):
         loader.load(ui_file,self)
         ui_file.close()
 
-class List_App(QWidget):
-    def __init__(self):
-        super(List_App,self).__init__()
-        self.setWindowTitle("List App")
+class Process(QWidget):
+    def __init__(self,Client):
+        super(Process,self).__init__()
+        self.client_ = Client
+        self.setWindowTitle("Process Running")
         self .load_ui()
-        self.list = self.findChild(QTableWidget,"list_app")
-        self.list.setColumnWidth(0,130)
+        self.name = []
+        self.id = []
+        self.count = []
 
     def load_ui(self):
         loader = QUiLoader()
@@ -133,6 +180,10 @@ class List_App(QWidget):
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file,self)
         ui_file.close()
+        self.list = self.findChild(QTableWidget,"tableWidget")
+        self.list.setColumnWidth(0,150)
+        self.list.setColumnWidth(1,150)
+        self.list.setColumnWidth(0,150)
 
         self.kbutton = self.findChild(QPushButton,"kill_Button")
         self.sbutton = self.findChild(QPushButton,"start_Button")
@@ -143,29 +194,80 @@ class List_App(QWidget):
         #hàm liên kết action khi kết nối được với server
         self.kbutton.clicked.connect(self.kill_app)
         self.sbutton.clicked.connect(self.start_app)
-        self.vbutton.clicked.connect(self.view_app)
+        self.vbutton.clicked.connect(self.view_start)
         self.dbutton.clicked.connect(self.delete_app)
 
     def delete_app(self):
-        self.list = self.findChild(QTableWidget,"list_app")
         self.list.clearContents()
 
+    def view_start(self):
+        process_(self.client_)
+        self.receive_process()
+        self.view_app()
     def view_app(self):
-        self.list = self.findChild(QTableWidget,"listapp")
-        row = 0
+        self.row = 0
+
+        #f = wmi.WMI()
+        # Printing the header for the later columns
+        print("Printing")
+        # Iterating through all the running processes
+        data = self.client_.recv(2048)
+        while True:
+        #for process in f.Win32_Process():
+         #   s1 = f"{process.ProcessID:<10}"
+          #  s2 = f"{process.Name:<20}"
+           # s3 = f"{process.ThreadCount:<10}"
+           self.client_.send(bytes('1','utf-8'))
+           s1 = data.decode()
+           self.id.append(str(s1))
+
+           data = self.client_.recv(2048)
+           self.client_.send(bytes('1','utf-8'))
+           s2 = data.decode()
+           self.name.append(str(s2))
+
+           data = self.client_.recv(2048)
+           self.client_.send(bytes('1','utf-8'))
+           s3 = data.decode()
+           self.count.append(str(s3))
+           print(str(s1) + str(s2) + str(s3))
+           self.row +=1
+           data = self.client_.recv(2048)
+            # Displaying the P_ID and P_Name of the process
+           if(data.decode()=="end"):
+               break
+        self.list.setRowCount(self.row)
+        for i in range(len(self.name)):
+            self.list.setItem(i,0,QTableWidgetItem(self.name[i]))
+            self.list.setItem(i,1,QTableWidgetItem(self.id[i]))
+            self.list.setItem(i,2,QTableWidgetItem(self.count[i]))
+            #print(self.id[i] + self.nam[i] + self.count[i])
+
+
 
 
     def kill_app(self):
-        self.kill_ = kill()
+        self.kill_ = kill(self.client_)
         self.kill_.show()
 
     def start_app(self):
         self.start_ = start()
         self.start_.show()
 
+    def receive_process(self):
+        while True:
+            try:
+                command = self.client_.recv(4096)
+                if (command.decode() == "runprocess"):
+                    return
+            except:
+                break
+
+
 class Keystroke(QWidget):
-    def __init__(self):
+    def __init__(self,Client):
         super(Keystroke,self).__init__()
+        self.client_ = Client
         self.setWindowTitle("Keystroke")
         self.load_ui()
 
@@ -190,8 +292,9 @@ class Keystroke(QWidget):
         self.list.clear()
 
 class Registry(QWidget):
-    def __init__(self):
+    def __init__(self,Client):
         super(Registry,self).__init__()
+        self.client_ = Client
         self.setWindowTitle("registry")
         self.load_ui()
 
@@ -202,8 +305,58 @@ class Registry(QWidget):
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file,self)
         ui_file.close()
+class Pic(QWidget):
+    def __init__(self,Client):
+        super(Pic,self).__init__()
+        self.client_ = Client
+        self.signal = True #do ket noi roi moi dung duoc chuc nang chup hinh
+        self.setWindowTitle("Picture")
+        self.load_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__),"pic.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file,self)
+        ui_file.close()
+
+        self.label = self.findChild(QLabel,"label")
+        self.label.setAutoFillBackground(True)
+        self.label.setScaledContents(False)
+        self.label.setText("")
+        self.takebutton = self.findChild(QPushButton,"pic_Button")
+        self.savebutton = self.findChild(QPushButton,"save_Button")
+
+        self.takebutton.clicked.connect(self.capture)
+        self.savebutton.clicked.connect(self.save)
 
 
+    def capture(self):
+        print_screen(self.client_)
+        receive_image(self.client_)
+        self.displayImg()
+
+    def displayImg(self):
+        img = (QPixmap("shot.jpg"))
+        scaled = img.scaled(self.label.size(),Qt.KeepAspectRatio)
+        self.label.setPixmap(scaled)
+        self.label.setScaledContents(True)
+
+    def save(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"Save screenshot as","","All Files (*);;Image Files (*jpg)",options = options)
+        #### write binary file
+
+        file = open(fileName,'wb')
+        f = open ("shot.jpg",'rb')
+        data = f.read()
+        file.write(data)
+        f.close()
+        file.close()
+
+#function client
 def create(ipline: str):
     PORT = 5656
     FORMAT = 'utf-8'
@@ -212,10 +365,42 @@ def create(ipline: str):
 
     Client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     Client.connect(ADDR)
+    return Client
+def receive_image(socket):
+    while True:
+        try:
+            command = socket.recv(4096)
+            if (command.decode() == "printscreen"):
+                data = socket.recv(4096)
+                size = int(data.decode())
+                recv_fz = 0
+                file = open('shot.jpg','wb')
+                while not recv_fz == size:       # receive binary data relatively
+                      if size - recv_fz > 4096:
+                         recv_data = socket.recv(4096)
+                         recv_fz +=len(recv_data)
+                      else:
+                         recv_data = socket.recv(size - recv_fz)
+                         recv_fz = Size
+                      file.write(recv_data)             #write all the received binary data to file
+                file.close()
+        except:
+            break
+
+
+def process_(socket):
+    socket.send(str.encode("runprocess"))
+
+def print_screen(socket):
+    socket.send(str.encode("printscreen"))
+
+def shut_down(socket):
+    socket.send(str.encode("shutdown"))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = client()
+    widget = Form()
     widget.show()
 
 
--- server/main.py
+++ server/main.py
@@ -3,15 +3,21 @@ import sys
 import os
 import socket
 import threading
+import time
+import signal
+import pyautogui
+import wmi
 
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton,QMessageBox
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
+BUFFER_SIZE = 4096
 
-class server(QWidget):
+class Form(QWidget):
     def __init__(self):
-        super(server, self).__init__()
+        super(Form, self).__init__()
+        self.server_ = None
         self.setWindowTitle("server")
         self.load_ui()
         self.set_Button()
@@ -19,7 +25,7 @@ class server(QWidget):
 
     def set_Button(self):
         self.button = self.findChild(QPushButton,"server_Button")
-        self.button.clicked.connect(create)
+        self.button.clicked.connect(self.create)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -28,22 +34,98 @@ class server(QWidget):
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
         ui_file.close()
+    def create(self):
+        if self.server_ == None:
+            self.server_ = Server()
+        elif self.server_ != None:
+            self.server_ = None
+            self.server = Server()
 
 
-def create():
-    PORT = 5656
-    SERVER = ""
-    ADDR = (SERVER,PORT)
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen(1)
-    conn, addr = server.accept()
+class Server:
+    def __init__(self,signal=False):
+        super(Server,self).__init__()
+        self.server = None
+        self.signal = signal
+        self.PORT = 5656
+        self.SERVER = ""
+        self.ADDR = (self.SERVER,self.PORT)
+        self.address= self.ADDR
+        self.create()
+        self.run()
+
+    def create(self):
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.server.bind(self.ADDR)
+        self.server.listen(1)
+        self.conn, self.addr = self.server.accept()
+        self.signal = True
+
+    def __str__(self):
+        return self.address
+
+    def run(self):
+        with self.conn:
+            while True:
+                data = self.conn.recv(4096)
+                if data.decode() == "shutdown":
+                    self.shut_down()
+                elif data.decode() == "printscreen":
+                    self.conn.send(str.encode("printscreen"))
+                    self.print_screen()
+                elif data.decode() == "runprocess":
+                    self.conn.send(str.encode("runprocess"))
+                    self.run_process()
+                if not data:
+                    break
+
+    def shut_down(self):
+        os.system("shutdown /s /t 30")
+    def run_process(self):
+        f = wmi.WMI()
+        for process in f.Win32_Process():
+            s1 = f"{process.ProcessID:<10}"
+            s2 = f"{process.Name:<20}"
+            s3 = f"{process.ThreadCount:<10}"
+            #self.conn.send(bytes(str(s1)))
+            self.conn.send(str.encode(s1))
+            self.conn.recv(1)
+
+            #self.conn.send(bytes(str(s2)))
+            self.conn.send(str.encode(s2))
+            self.conn.recv(1)
+
+            #self.conn.send(bytes(str(s3))
+            self.conn.send(str.encode(s3))
+            self.conn.recv(1)
 
 
+            print(str(s1)+str(s2)+str(s3))
+        self.conn.send(str.encode("end"))
+
+
+
+    def print_screen(self):
+        img = pyautogui.screenshot()
+        img.save(r'shot.jpg')
+        file = open("shot.jpg",'rb')
+        size = os.stat('shot.jpg').st_size
+        self.conn.send(str.encode(str(size)))
+
+        while True:
+            sendData = file.read(4096)
+            if not sendData: #send toi khi khong con data de gui thi break
+                break
+            self.conn.send(sendData)
+
+        file.close()
+        os.remove("shot.jpg")
+
+def kill(socket):
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    widget = server()
+    widget = Form()
     widget.show()
     sys.exit(app.exec_())
